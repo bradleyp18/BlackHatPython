c35dd3c8331a6724d7bca12700cbc684654c23d1
Bradley Powell <bradley.powell18@gmail.com>
2022-04-25 19:23:00 -0500

    TCP Server

---
+++ Sockets/TCP_Server.py
@@ -0,0 +1,29 @@
+import socket
+import threading
+
+# IP Address
+HOST = '0.0.0.0'
+# IP Port
+PORT = '42069'
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST,PORT))
+    # Number of listening connections
+    server.listen(5)
+    print(f"Listening on {HOST}:{PORT}")
+
+    while True:
+        conn, addr = server.accept()
+        print(f"[*] Accepted connection from {addr[0]}:{addr[1]}")
+        client_handler = threading.Thread(target=handle_client, args = (client,))
+        client_handler.start()
+
+def handle_client(client_socket):
+    with client_socket as socket:
+        request = socket.recv(1024)
+        print(f" Received: {request.decode()}")
+        socket.send(b'ACK')
+
+if __name__ == "__main__":
+    main()
